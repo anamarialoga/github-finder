7666ceda7dd898b8025980fbd3b23c66d1a415e0
anamarialoga <anamarialogaa82@gmail.com>
2022-02-11 22:06:12 +0200

    setting up components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Navbar } from './components/layout/Navbar';
+import { Footer} from './components/layout/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <BrowserRouter>
+    <div className='flex flex-col justify-between h-screen'>
+      <Navbar />
+      <main className='container mx-auto px-3 pb-12'>
+        <Routes>
+          <Route path = "/" element={<Home/>}/>
+          <Route path = "/about" element={<About/>}/>
+          <Route path = "/notfound" element={<NotFound />}/>
+          <Route path = "/*" element={<NotFound />}/>
+        </Routes>
+      </main>
+      <Footer />
     </div>
+    </BrowserRouter>
   );
 }
 
